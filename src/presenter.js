4cbd995bf2e0d833dcf1620c7a9ceba1f5cc9063
Mateo Gandarillas <mateoganav@gmail.com>
2025-02-14 19:40:34 -0400

    feat: Saludar en Español o Inglés

--- src/presenter.js
+++ src/presenter.js
@@ -1,6 +1,7 @@
 const nombre = document.querySelector("#nombre");
 const genero = document.querySelector("#genero");
 const edad = document.querySelector("#edad");
+const idioma = document.querySelector("#idioma");
 const form = document.querySelector("#saludo-form");
 const div = document.querySelector("#saludo-div");
 
@@ -12,39 +13,44 @@ form.addEventListener("submit", (event) => {
   const nombreIn = nombre.value.trim(); 
   const generoIn = genero.value;
   const edadIn = parseInt(edad.value);
+  const idiomaIn = idioma.value;
 
   let hora;
-  if (horaActual < 12) {
-    hora = "Buenos días";
-  } else if (horaActual < 18) {
-    hora = "Buenas tardes";
-  } else {
-    hora = "Buenas noches";
+  if (idiomaIn == "espanol"){
+    if (horaActual < 12) {
+      hora = "Buenos días";
+    } else if (horaActual < 18) {
+      hora = "Buenas tardes";
+    } else {
+      hora = "Buenas noches";
+    }
   }
+  else if (idiomaIn == "ingles"){
+    if (horaActual < 12) {
+      hora = "Good morning";
+    } else if (horaActual < 18) {
+      hora = "Good evening";
+    } else {
+      hora = "Good night";
+    }
+  }
+
 
   let saludoG = "";
-  if (generoIn === "masculino") 
-  {
-    if(edadIn > 30)
-    {
-      saludoG = "señor";
+  if (idiomaIn == "espanol") {
+    if (generoIn == "masculino") {
+      saludoG = edadIn > 30 ? "señor" : "jovencito";
+    } else if (generoIn == "femenino") {
+      saludoG = edadIn > 30 ? "señora" : "jovencita";
     }
-    else
-    {
-      saludoG = "joven";
-    }  
   } 
-  else if (generoIn === "femenino") 
-  {
-    if(edadIn > 30)
-      {
-        saludoG = "señora";
-      }
-      else
-      {
-        saludoG = "joven";
-      }  
+  else {
+    if (generoIn == "masculino") {
+      saludoG = edadIn > 30 ? "Mr." : "young man";
+    } else if (generoIn == "femenino") {
+      saludoG = edadIn > 30 ? "Mrs." : "young lady";
+    }
   }
-
+    
   div.innerHTML = `<p>${hora} ${saludoG} ${nombreIn}</p>`;
 });
