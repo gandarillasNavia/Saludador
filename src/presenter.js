e29496b162078971d9f14e5e4f9029eae55fb2b5
Mateo Gandarillas <mateoganav@gmail.com>
2025-02-14 19:11:05 -0400

    feat: Saludar de acuerdo a la hora

--- src/presenter.js
+++ src/presenter.js
@@ -2,8 +2,20 @@ const nombre= document.querySelector("#nombre");
 const form = document.querySelector("#saludo-form");
 const div = document.querySelector("#saludo-div");
 
+let fechaActual = new Date();
+let horaActual = fechaActual.getHours();
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const nombreIn = nombre.value.trim(); 
-  div.innerHTML = `<p>Hola ${nombreIn}</p>`;
+  let hora;
+
+  if (horaActual < 12) {
+    hora = "Buenos días";
+  } else if (horaActual < 18) {
+    hora = "Buenas tardes";
+  } else {
+    hora = "Buenas noches";
+  }
+  div.innerHTML = `<p>${hora} ${nombreIn}</p>`;
 });
